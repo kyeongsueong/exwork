b892f947ec5e1c353a45b4cfe4ea14e1cb41a749
kyeongsueong <67498838+kyeongsueong@users.noreply.github.com>
2020-10-23 19:31:32 +0900

    Update carlaw.js

--- carlaw.js
+++ carlaw.js
@@ -117,11 +117,11 @@ class mycanvas{
 	function opneDown() {
 		document.querySelector(".divid").style.zIndex = "2";
 		document.getElementById('link').style.zIndex = "-1";
-                document.querySelector(".divid").style.display = 'block;
+                //document.querySelector(".divid").style.display = 'block;
 	}
 	function closeDown() {
 		document.querySelector(".divid").style.zIndex = "-1";
-                document.querySelector(".divid").style.display = 'none';
+                //document.querySelector(".divid").style.display = 'none';
 	}
 	let focusRect = {
 		x: 130,
