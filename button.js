783b4824f0e4b4fd2641d2bc345e3370c136d635
kyeongsueong <67498838+kyeongsueong@users.noreply.github.com>
2020-10-25 11:29:35 +0900

    Update button.js

--- button.js
+++ button.js
@@ -44,7 +44,7 @@ function exwaylocate(x,y) {
 			exwaydata();
 			return this.exl[1] + this.exl[2] + "km";
 		}
-		if( this.i == exlocate.length ){ 
+		else if( this.i == exlocate.length ){ 
 			return `정보없음 // 위도: ${x} //경도: ${y}`;
 		}
 	}
